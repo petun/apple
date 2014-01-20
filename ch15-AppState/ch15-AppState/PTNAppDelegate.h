c674fef72084b45b6e7631a82a7453be4e5487a3
Admin <admin@Admins-Mac.local>
2014-01-20 09:54:52 +0400

    ch15-AppLifeCyrcle

---
+++ ch15-AppState/ch15-AppState/PTNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTNAppDelegate.h
+//  ch15-AppState
+//
+//  Created by Admin on 20.01.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ch15-AppState/ch15-AppState/PTNViewController.h
@@ -0,0 +1,21 @@
+//
+//  PTNViewController.h
+//  ch15-AppState
+//
+//  Created by Admin on 20.01.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTNViewController : UIViewController
+
+@property (strong,nonatomic) UILabel *label;
+
+@property (strong,nonatomic) UIImage *smiley;
+@property (strong,nonatomic) UIImageView *smileyView;
+
+@property (strong,nonatomic) UISegmentedControl *segmentedControl;
+
+
+@end
